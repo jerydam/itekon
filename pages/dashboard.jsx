fac0422558ba65ac5b7bb7adbb455a2823766916
Jerydam <jerydan148@gmail.com>
2023-10-22 01:27:14 +0100

    location identify

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,4 +1,6 @@
 import Sidebar from "@/components/sidebar";
+import MapComponent from "@/components/map";
+
 
 const Dashboard = () => {
   return (
@@ -51,16 +53,8 @@ const Dashboard = () => {
 
         <div className="w-full flex  justify-between gap-4 px-5">
         <div className="w-full border-2 h-60 rounded">
-  <iframe
-    title="Location Map"
-    width="100%"
-    height="100%"
-    frameBorder="0"
-    style={{ border: 0 }}
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3224.8646256490914!2d-122.08462808469962!3d37.42291067985082!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7e2f256f59a5%3A0xe557622f2030a948!2sGoogle%20Plex!5e0!3m2!1sen!2sus!4v1632328547239!5m2!1sen!2sus"
-    allowFullScreen=""
-  ></iframe>
-</div>
+        <MapComponent />
+        </div>
 
           <div className="w-full border-2 h-60 rounded"></div>
         </div>
