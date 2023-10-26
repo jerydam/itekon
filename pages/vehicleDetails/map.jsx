45e4b3529af98056bebb7a73c87f9d172c16b4ba
Jerydam <jerydan148@gmail.com>
2023-10-26 01:20:44 +0100

    omoh

--- pages/vehicleDetails/map.jsx
+++ pages/vehicleDetails/map.jsx
@@ -1,37 +1,46 @@
-'use client'
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import 'styles/global.css';
+// import React, { useState } from 'react';
+// import ReactMapGL, { Marker } from 'react-map-gl';
 
-const MapComponent = ({ onLocationSelect }) => {
-  const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-  });
+// const MapComponent = ({ onLocationSelect }) => {
+//   const [viewport, setViewport] = useState({
+//     latitude: 37.7577,
+//     longitude: -122.4376,
+//     zoom: 8,
+//   });
 
-  const handleMapClick = (event) => {
-    const [longitude, latitude] = event.lngLat;
-    onLocationSelect && onLocationSelect({ latitude, longitude });
-  };
+//   const handleMapClick = (event) => {
+//     const [longitude, latitude] = event.lngLat;
+//     onLocationSelect({ latitude, longitude });
+//   };
 
+//   return (
+//     <div style={{ height: '80vh', width: '100%' }}>
+//       <ReactMapGL
+//         {...viewport}
+//         width="100%"
+//         height="100%"
+//         mapStyle="mapbox://styles/mapbox/streets-v11"
+//         onViewportChange={(viewport) => setViewport(viewport)}
+//         onClick={handleMapClick}
+//         mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+//       >
+//         <Marker latitude={viewport.latitude} longitude={viewport.longitude}>
+//           <div>You are here</div>
+//         </Marker>
+//       </ReactMapGL>
+//     </div>
+//   );
+// };
+
+// export default MapComponent;
+
+
+import React from 'react'
+
+const map = () => {
   return (
-    <div className="h-80vh w-full">
-      <ReactMapGL
-        {...viewport}
-        width="100%"
-        height="100%"
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onViewportChange={(viewport) => setViewport(viewport)}
-        onClick={handleMapClick}
-        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN || 'YOUR_MAPBOX_TOKEN'}
-      >
-        <Marker latitude={viewport.latitude} longitude={viewport.longitude}>
-          <div className="bg-blue-500 text-white p-2 rounded">You are here</div>
-        </Marker>
-      </ReactMapGL>
-    </div>
-  );
-};
+    <div>map</div>
+  )
+}
 
-export default MapComponent;
+export default map
