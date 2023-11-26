3816dbc2e63e1ad9b81ef5c8dcd0a1ac941853ac
Jerydam <jerydan148@gmail.com>
2023-11-26 23:23:06 +0100

    well done

--- app/driver/page.jsx
+++ app/driver/page.jsx
@@ -12,9 +12,16 @@ const DriverList = ({ onDrive, onRemove, onEdit, onAssignVehicle }) => {
   const [showDrive, setShowDrive] = useState(false);
   const [showRemove, setShowRemove] = useState(false);
 
-  const userToken = sessionStorage.getItem('authToken');
-  console.log(userToken);
-  const userId = sessionStorage.getItem('id');
+  let userId;
+  let userToken;
+
+  useEffect(() => {
+    if (typeof sessionStorage !== 'undefined') {
+      const userToken = sessionStorage.getItem('authToken');
+      console.log(userToken);
+      const userId = sessionStorage.getItem('id');
+    }
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
